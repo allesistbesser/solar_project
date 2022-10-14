8c30c009bc0fab1ba80db444c55e380903306e3d
allesistbesser <mtepebas46@gmail.com>
2022-10-14 12:33:33 +0200

    edit shop button, aadd shop info

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ export const backend_path = "https://minisolarsysteme.de"
 
 export let mail = "minisolarsysteme@gmail.com"
 export let adres = "Asper str. 6 - 32108 Bad Salzuflen"
-export let phone = "+49 111 123 45 67"
+export let phone = ""
 
 export let facebook = "http://www.facebook.com"
 export let twitter = "http://www.twitter.com"
